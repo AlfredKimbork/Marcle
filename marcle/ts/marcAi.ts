55909de6d53919091791bd86cfdbddc477798e07
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2025-12-11 08:37:16 +0100

    a few bug fixes.. I think

--- marcle/ts/marcAi.ts
+++ marcle/ts/marcAi.ts
@@ -1,5 +1,5 @@
 import { possibleWords } from "./possibleWords/words.js";
-import { addLetter, moveMarked, checkWord, yellows, greens, greys, word, row, gameStatus } from "./wordleLogic.js";
+import { addLetter, moveMarked, checkWord, yellows, greens, greys, gameStatus } from "./wordleLogic.js";
 import { thinkingDelay, writingDelay, marcActive, setMarcSpeed } from "./initiateGame.js";
 export const marcBtn = document.querySelector("#marc-btn") as HTMLButtonElement
 export const speedBtn = document.querySelector("#speed-btn") as HTMLButtonElement
@@ -7,12 +7,14 @@ export const askMarc = document.querySelector("#ask-marc-btn") as HTMLButtonElem
 let possibleGuesses = possibleWords
 
 let i = 0;
+let guesses = 0
 let guessIndex: number;
 let marcGuess: string;
 export let marcThinking: number
 
 const getGuess = () => {
-        return possibleGuesses[Math.round(Math.random() * (possibleGuesses.length - 1))]
+        let i = Math.round(Math.random() * (possibleGuesses.length - 1))
+        return possibleGuesses[i];
     }
 
 const writeGuess = () => {
@@ -25,6 +27,7 @@ const writeGuess = () => {
         i++
     } else if (marcActive) {
         checkWord()
+        possibleGuesses.splice(guessIndex, 1)
         moveMarked()
         i = 0
         
@@ -40,64 +43,152 @@ const writeGuess = () => {
 export const checkMarcGuess = () => {
     marcGuess = getGuess()
     guessIndex = possibleGuesses.indexOf(marcGuess);
-    possibleGuesses.splice(guessIndex, 1)
+    // possibleGuesses.splice(guessIndex, 1)
 
-    if (greens.length > 0) checkGreens()
-    else if (yellows.length > 0 && greens.length <= 0) checkYellows()
-    else {
-        writeGuess()
-    };
-}
+    if (guesses > 1) {
+        checkIfValid()
 
-const checkGreens = () => {
-    let valid = 0
-    for (let j = 0; j < greens.length; j++) {
-        if (greens[j].letter == marcGuess[greens[j].position]) {
-            valid++
-            if(valid == greens.length) {
-                if (yellows.length > 0) checkYellows()
-                    else if (greys.length > 0) checkGreys()
-                else {
-                    writeGuess()
-                }
-            }
-        } else {
-            checkMarcGuess()
-            break;
-        }
-    }
-}
-
-const checkYellows = () => {
-    let valid = 0
-    for (let j = 0; j < yellows.length; j++) {
-        if (marcGuess.includes(yellows[j].letter) && yellows[j].letter != marcGuess[yellows[j].position]) {
-            valid++
-            if(valid == yellows.length) {
-                if (greys.length > 0) checkGreys()
-                else {
-                    writeGuess()
-                }
-            }
-        } else {
-            checkMarcGuess()
-            break;
-        }
-    }
-}
-
-const checkGreys = () => {
-    let valid = 0
-    for (let j = 0; j < greys.length; j++) {
-        if (greys[j].letter != marcGuess[greys[j].position]) {
-            valid++
-            if(valid == greys.length) {
+    } else {
+        let returnStatement = true
+        for (let greyIndex = 0; greyIndex < greys.length; greyIndex++) {
+            const grey = greys[greyIndex];
+            if (marcGuess.includes(grey.letter)) {
+                returnStatement = false;
+                break;
+            }}
+            if (returnStatement) {
                 writeGuess()
+                guesses++
             }
-        } else {
-            checkMarcGuess()
+            else {
+                checkMarcGuess()
+            }
+    }
+
+    // if (guesses < 2 && greys.length > 0) checkGreys()
+    //     else if (greens.length > 0) checkGreens()
+    //         else if (yellows.length > 0 && greens.length <= 0) checkYellows()
+    //             else {
+    //                 writeGuess()
+    //             };
+}
+
+// const checkGreens = () => {
+//     let valid = 0
+//     for (let j = 0; j < greens.length; j++) {
+//         if (greens[j].letter == marcGuess[greens[j].position]) {
+//             valid++
+//             if(valid == greens.length) {
+//                 if (yellows.length > 0) checkYellows()
+//                     else if (greys.length > 0) checkGreys()
+//                 else {
+//                     writeGuess()
+//                 }
+//             }
+//         } else {
+//             checkMarcGuess()
+//             break;
+//         }
+//     }
+// }
+
+// const checkYellows = () => {
+//     let valid = 0
+//     for (let j = 0; j < yellows.length; j++) {
+//         if (marcGuess.includes(yellows[j].letter) && yellows[j].letter != marcGuess[yellows[j].position]) {
+//             valid++
+//             if(valid == yellows.length) {
+//                 if (greys.length > 0) checkGreys()
+//                 else {
+//                     writeGuess()
+//                 }
+//             }
+//         } else {
+//             checkMarcGuess()
+//             break;
+//         }
+//     }
+// }
+
+const checkIfValid = () => {
+    let returnStatement = true
+    console.log("check", marcGuess)
+        
+    for (let greyIndex = 0; greyIndex < greys.length; greyIndex++) {
+        console.log(greyIndex)
+        const grey = greys[greyIndex];
+        // if(greyIndex == 0)console.log(greens)
+            
+        // if any green is present
+        if(greens.length > 0) for (let greenIndex = 0; greenIndex < greens.length; greenIndex++) {
+            // if(greyIndex == 0)console.log("green is present")
+            const green = greens[greenIndex];
+            if (!marcGuess.includes(green.letter)) {
+                returnStatement = false;
+                break;
+            } else if (green.letter != marcGuess[green.position]) {
+                returnStatement = false;
+                console.log(green.letter, marcGuess[green.position])
+                break;
+            } else console.log(green.letter, marcGuess[green.position])
+            // if any green + yellow is present
+            if(yellows.length > 0) for (let yellowIndex = 0; yellowIndex < yellows.length; yellowIndex++) {
+                const yellow = yellows[yellowIndex];
+                if (!marcGuess.includes(yellow.letter)) {
+                    returnStatement = false;
+                    break;
+                } else if (yellow.letter == marcGuess[yellow.position]) {
+                    returnStatement = false;
+                    break;
+                }
+                // if grey letter is equal to yellow or green
+                if (grey.letter == yellow.letter || grey.letter == green.letter) {
+                    returnStatement = true
+                    break;
+                // if grey letter is NOT equal to yellow or green
+                } else if (grey.letter != yellow.letter && marcGuess.includes(grey.letter) 
+                    || grey.letter != green.letter && marcGuess.includes(grey.letter)) {
+                    returnStatement = false;
+                } 
+                // if grey is NOT equal to green
+            } else if (grey.letter != green.letter && marcGuess.includes(grey.letter)) {
+                returnStatement = false;
+                break;
+                // grey is NOT equal to green
+            }
+            if(returnStatement) break
+        // if no greens but yellows
+        } else if (yellows.length > 0) for (let yellowIndex = 0; yellowIndex < yellows.length; yellowIndex++) {
+            const yellow = yellows[yellowIndex];
+            if (!marcGuess.includes(yellow.letter)) {
+                returnStatement = false;
+                break;
+            } 
+            if (yellow.letter == marcGuess[yellow.position]) {
+                returnStatement = false;
+                break;
+            }
+            // if grey letter is equal to yellow
+            if (grey.letter == yellow.letter) {
+                returnStatement = true
+                break;
+            // if grey letter is NOT equal to yellow
+            } else if (marcGuess.includes(grey.letter)) {
+                returnStatement = false;
+            }
+            if(returnStatement) break
+        // if no yellows or greens
+        } else if (marcGuess.includes(grey.letter)) {
+            returnStatement = false;
             break;
         }
+    }
+    
+    if(returnStatement) {
+        writeGuess() 
+    } else {
+        possibleGuesses.splice(guessIndex, 1);
+        checkMarcGuess() ;
     }
 }
 
--- marcle/ts/wordleLogic.ts
+++ marcle/ts/wordleLogic.ts
@@ -1,8 +1,9 @@
 import { marcActive } from "./initiateGame.js";
-import { getWord } from "./possibleWords/words.js";
+import { getWord, possibleWords } from "./possibleWords/words.js";
 export const keyboardBtns = document.querySelectorAll(".keyboard") as NodeListOf<HTMLButtonElement>;
 let currentCell: HTMLSpanElement | undefined
 let column = 0;
+let possibleWord = possibleWords
 export let gameStatus: string
 export let row = 0;
 
@@ -109,6 +110,8 @@ export let row = 0;
     export let yellows: {"letter":string, "position": number}[] = [];
     export let greys: {"letter":string, "position": number}[] = [];
 
+    console.log(word, possibleWord.indexOf(word))
+
     export const checkWord = () => {
         let guessedLetters = document.querySelectorAll(`.row-${row}`) as NodeListOf<HTMLSpanElement>;
         let guessedWord = "";
@@ -116,174 +119,186 @@ export let row = 0;
         
         let j = 0;
         let amountDeleted = 0;
-        
-        guessedLetters.forEach((guessedLetter, i) => {
-            guessedWord += guessedLetter.innerText;
-            
-            if (guessedLetter.innerText == word[i]) {
-                guessedLetter.classList.add("green", "check");
-                setTimeout(() => {
-                    guessedLetter.classList.remove("check");
-                }, 500)
-                
-                keyboardBtns.forEach(btn => {
-                    if (btn.id == `Key${guessedLetter.innerText.toLocaleUpperCase()}`) btn.classList.add("green", "check");
-                })
-                
-                missingLetters = missingLetters.replace(guessedWord[j - amountDeleted], "");
-                guessedWord = guessedWord.replace(guessedWord[j - amountDeleted], "");
-                greens.push({"letter": guessedLetter.innerText, "position": i});
-                
-                
-                amountDeleted++;
-                j++;
-            } else if (missingLetters.includes(guessedLetter.innerText)) {
-                guessedLetter.classList.add("yellow", "check");
-                setTimeout(() => {
-                    guessedLetter.classList.remove("check");
-                }, 500)
-                keyboardBtns.forEach(btn => {
-                    if (btn.id == `Key${guessedLetter.innerText.toLocaleUpperCase()}`) btn.classList.add("yellow", "check");
-                })
-                
-                missingLetters = missingLetters.replace(guessedWord[j - amountDeleted], " ");
-                guessedWord = guessedWord.replace(guessedWord[j - amountDeleted], "");
-                
-                yellows.push({"letter": guessedLetter.innerText, "position": i});
-                
-                j++
-                amountDeleted++
-            } else {
-                guessedLetter.classList.add("grey", "check");
-                setTimeout(() => {
-                    guessedLetter.classList.remove("check");
-                }, 500)
-                keyboardBtns.forEach(btn => {
-                    greys.push({"letter": guessedLetter.innerText, "position": i})
-                    if (btn.id == `Key${guessedLetter.innerText.toLocaleUpperCase()}`) btn.classList.add("grey", "check");
-                });
-                
-                j++
-            }
-        });
-        
-        keyboardBtns.forEach(btn => {
-            if (btn.id == "enter") btn.classList.add("check");
-            setTimeout(() => {
-                btn.classList.remove("check");
-            }, 500)
-        })
 
         guessedLetters.forEach((guessedLetter) => {
             guessedWord += guessedLetter.innerText;
         });
-        
-        let winstreak: number
-        if (guessedWord == word) {
-            gameStatus = "won"
 
-            localStorage.getItem("winstreak") ? winstreak = JSON.parse(localStorage.getItem("winstreak")!) + 1 
-                : winstreak = 1
-            localStorage.setItem("winstreak", JSON.stringify(winstreak));
+        if(possibleWord.indexOf(guessedWord) >= 0) {
+            guessedLetters.forEach((guessedLetter, i) => {
+                
+                if (guessedLetter.innerText == word[i]) {
+                    guessedLetter.classList.add("green", "check");
+                    setTimeout(() => {
+                        guessedLetter.classList.remove("check");
+                    }, 500)
+                    
+                    keyboardBtns.forEach(btn => {
+                        if (btn.id == `Key${guessedLetter.innerText.toLocaleUpperCase()}`) btn.classList.add("green", "check");
+                    })
+                    
+                    missingLetters = missingLetters.replace(guessedWord[i - amountDeleted], "");
+                    guessedWord = guessedWord.replace(guessedWord[i - amountDeleted], "");
+                    greens.push({"letter": guessedLetter.innerText, "position": i});
+                    
+                    amountDeleted++;
+                    j++;
+                } 
+            });
+            guessedLetters.forEach((guessedLetter, i) => {
+                if (missingLetters.includes(guessedLetter.innerText)) {
+                    guessedLetter.classList.add("yellow", "check");
+                    setTimeout(() => {
+                        guessedLetter.classList.remove("check");
+                    }, 500)
+                    keyboardBtns.forEach(btn => {
+                        if (btn.id == `Key${guessedLetter.innerText.toLocaleUpperCase()}`) btn.classList.add("yellow", "check");
+                    })
+                    
+                    missingLetters = missingLetters.replace(guessedWord[i - amountDeleted], " ");
+                    guessedWord = guessedWord.replace(guessedWord[i - amountDeleted], "");
+                    
+                    yellows.push({"letter": guessedLetter.innerText, "position": i});
+                    
+                    j++
+                    amountDeleted++
+                } else {
+                    guessedLetter.classList.add("grey", "check");
+                    greys.push({"letter": guessedLetter.innerText, "position": i})
+                    setTimeout(() => {
+                        guessedLetter.classList.remove("check");
+                    }, 500)
+                    keyboardBtns.forEach(btn => {
+                        if (btn.id == `Key${guessedLetter.innerText.toLocaleUpperCase()}`) btn.classList.add("grey", "check");
+                    });
+                    
+                    j++
+                }
+            });
+                
+                keyboardBtns.forEach(btn => {
+                    if (btn.id == "enter") btn.classList.add("check");
+                    setTimeout(() => {
+                    btn.classList.remove("check");
+                }, 500)
+            })
+
+            guessedLetters.forEach((guessedLetter) => {
+                guessedWord += guessedLetter.innerText;
+                });
             
-            currentCell!.classList.remove("current");
-            currentCell = undefined;
-
-
-            document.removeEventListener("keyup", activeKeyboard)
-            keyboardBtns.forEach(keyboardBtn => {
-                keyboardBtn.removeEventListener("click", activeKeyboardFinger);
-            });
-
-            endscreen.innerHTML = `
-                <h2>You got the word!</h2>
-                <p>the word was <span class="bold">${word}</span>!</p>
-                <p>you now have a winstreak of ${winstreak}</p>
-            `
-        } else if(row == 5) {
-            gameStatus = "lost"
-
-            localStorage.getItem("winstreak") ? winstreak = JSON.parse(localStorage.getItem("winstreak")!) 
-                : winstreak = 0
-
-            currentCell!.classList.remove("current");
-            currentCell = undefined;
-
-            document.removeEventListener("keyup", activeKeyboard)
-            keyboardBtns.forEach(keyboardBtn => {
-                keyboardBtn.removeEventListener("click", activeKeyboardFinger);
-            });
-
-            endscreen.innerHTML = `
+            let winstreak: number
+                if (guessedWord == word) {
+                    gameStatus = "won"
+                    
+                    localStorage.getItem("winstreak") ? winstreak = JSON.parse(localStorage.getItem("winstreak")!) + 1 
+                    : winstreak = 1
+                    localStorage.setItem("winstreak", JSON.stringify(winstreak));
+                    
+                    currentCell!.classList.remove("current");
+                    currentCell = undefined;
+                    
+                    
+                    document.removeEventListener("keyup", activeKeyboard)
+                    keyboardBtns.forEach(keyboardBtn => {
+                        keyboardBtn.removeEventListener("click", activeKeyboardFinger);
+                    });
+                    
+                    endscreen.innerHTML = `
+                    <h2>You got the word!</h2>
+                    <p>the word was <span class="bold">${word}</span>!</p>
+                    <p>you now have a winstreak of ${winstreak}</p>
+                    `
+                } else if(row == 5) {
+                    gameStatus = "lost"
+                    
+                    localStorage.getItem("winstreak") ? winstreak = JSON.parse(localStorage.getItem("winstreak")!) 
+                    : winstreak = 0
+                    
+                    currentCell!.classList.remove("current");
+                    currentCell = undefined;
+                    
+                document.removeEventListener("keyup", activeKeyboard)
+                keyboardBtns.forEach(keyboardBtn => {
+                    keyboardBtn.removeEventListener("click", activeKeyboardFinger);
+                });
+                
+                endscreen.innerHTML = `
                 <h2>You didn't get the word!</h2>
                 <p>the word was <span class="bold">${word}</span>!</p>
                 <p>you had a winstreak of ${winstreak}</p>
-            `
-
-            winstreak = 0
-            localStorage.setItem("winstreak", JSON.stringify(winstreak));
-        } else{
-            row++;
-            column = 0;
-        }
-
-        const clearBoard = (nodes: NodeListOf<HTMLSpanElement>, nodeIndex: number) => {
-            if (nodeIndex < nodes.length) {
-                nodes[nodeIndex].classList.add("check");
-                nodes[nodeIndex].classList.remove("green", "yellow", "grey");
-
-                if (nodes[nodeIndex].classList.contains("cell")) nodes[nodeIndex].innerHTML = ""
-
-                setTimeout(() => {
-                    if (nodes[nodeIndex]) nodes[nodeIndex].classList.remove("green", "yellow", "grey");
-                }, 500)
-                nodeIndex++
-                setTimeout(() => clearBoard(nodes, nodeIndex), 25)
+                `
+                
+                winstreak = 0
+                localStorage.setItem("winstreak", JSON.stringify(winstreak));
+            } else{
+                row++;
+                column = 0;
             }
-        }
-
-        const resetGame = (resetBtn: HTMLButtonElement, clearBoard: Function) => {
-            const cells = document.querySelectorAll(".cell") as NodeListOf<HTMLSpanElement>;
-            let cellIndex = 0
-            let keyboardBtnIndex = 0
-
-            endscreen.setAttribute("style", "animation: slide-out ease-in-out .75s;");
             
-            resetBtn.classList.add("check");
-            setTimeout(() => {
-                resetBtn.classList.remove("check");
-                setTimeout(() => {
-                    endscreen.close();
-                    clearBoard(cells, cellIndex);
-                    clearBoard(keyboardBtns, keyboardBtnIndex);
+            const clearBoard = (nodes: NodeListOf<HTMLSpanElement>, nodeIndex: number) => {
+                if (nodeIndex < nodes.length) {
+                    nodes[nodeIndex].classList.add("check");
+                    nodes[nodeIndex].classList.remove("green", "yellow", "grey");
                     
-                }, 250);
-            }, 500);
-
-            setTimeout(() => {
-                window.location.reload()
-            }, 2000);
+                    if (nodes[nodeIndex].classList.contains("cell")) nodes[nodeIndex].innerHTML = ""
+                    
+                    setTimeout(() => {
+                        if (nodes[nodeIndex]) nodes[nodeIndex].classList.remove("green", "yellow", "grey");
+                    }, 500)
+                    nodeIndex++
+                    setTimeout(() => clearBoard(nodes, nodeIndex), 25)
+                }
+            }
             
-        }
-
-        if (gameStatus) {
-            const resetBtn = document.createElement("button");
-            resetBtn.classList.add("--btn");
-            resetBtn.innerText = "Reset";
-
-            resetBtn.addEventListener("click", () => {
-                resetGame(resetBtn, clearBoard)
+            const resetGame = (resetBtn: HTMLButtonElement, clearBoard: Function) => {
+                const cells = document.querySelectorAll(".cell") as NodeListOf<HTMLSpanElement>;
+                let cellIndex = 0
+                let keyboardBtnIndex = 0
+                
+                endscreen.setAttribute("style", "animation: slide-out ease-in-out .75s;");
+                
+                resetBtn.classList.add("check");
+                setTimeout(() => {
+                    resetBtn.classList.remove("check");
+                    setTimeout(() => {
+                        endscreen.close();
+                        clearBoard(cells, cellIndex);
+                        clearBoard(keyboardBtns, keyboardBtnIndex);
+                            
+                    }, 250);
+                }, 500);
+                
+                setTimeout(() => {
+                    window.location.reload()
+                }, 2000);
+                
+                
+                }
+            if (gameStatus) {
+                const resetBtn = document.createElement("button");
+                resetBtn.classList.add("--btn");
+                resetBtn.innerText = "Reset";
+                
+                resetBtn.addEventListener("click", () => {
+                    resetGame(resetBtn, clearBoard)
+                });
+                
+                if(marcActive == true) setTimeout(() =>resetGame(resetBtn, clearBoard), 2500);
+                
+                endscreen.append(resetBtn);
+                endscreen.showModal();
+            }   
+        } else {
+            const currentRow = document.querySelectorAll(`.row-${row}`) as NodeListOf<HTMLSpanElement>;
+            currentRow.forEach(cell => {
+                cell.style.transform = "translateX(.75rem)"
+                setTimeout(() => {
+                cell.style.transform = "translateX(-.75rem)"
+                    setTimeout(() => {
+                        cell.style.transform = "translateX(0)"
+                        },100)
+                },100)
             });
-
-            if(marcActive == true) setTimeout(() =>resetGame(resetBtn, clearBoard), 2500);
-
-            endscreen.append(resetBtn);
-            endscreen.showModal();
         }
     }
-
-// win & loose con
-    
-
-// const alphabets = [...Array(26).keys()].map((n) => String.fromCharCode(97 + n));
-// console.log(alphabets);
